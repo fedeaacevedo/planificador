2da6fa3b54db8e31de75b8dbfb602d82d63cea6f
Federico <federicoaacevedo@hotmail.com>
2022-02-14 01:08:11 -0300

    Graficos & Control Presupuesto

--- src/components/NuevoPresupuesto.js
+++ src/components/NuevoPresupuesto.js
@@ -1,36 +1,71 @@
-import React from 'react'
-import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native'
+import globalStyles from '../styles'
 
-const NuevoPresupuesto = () => {
-  return (
-    
-    <View style={styles.contenedor}>
-        <Text>
-            Definir Presupuesto
-        </Text>
+const NuevoPresupuesto = ({ handleNuevoPresupuesto }) => {
 
-        <TextInput
-        
-        />
 
-        <Pressable>
-            <Text>Agregar Presupuesto</Text>
-        </Pressable>
+    const [presupuesto, setPresupuesto] = useState(0)
 
-    </View>
-    
-  )
+    return (
+
+
+        <View style={styles.contenedor}>
+            <Text style={styles.label}>
+                Definir Presupuesto
+            </Text>
+
+            <TextInput
+                keyboardType='numeric'
+                placeholder='Agrega tu presupuesto:
+            Ej. 300'
+                style={styles.input}
+                value={presupuesto.toString()}
+                onChangeText={setPresupuesto}
+            />
+
+            <Pressable
+                style={styles.boton}
+                onPress={() =>
+                    handleNuevoPresupuesto(presupuesto)}
+            >
+                <Text style={styles.botonTexto}>Agregar Presupuesto</Text>
+            </Pressable>
+
+        </View>
+
+    )
 }
 
 const styles = StyleSheet.create({
-    contenedor:{
-        backgroundColor:'#FFF',
-        marginHorizontal: 10,
-        marginVertical: 20,
+    contenedor: {
+        ...globalStyles.contenedor
+    },
+    label: {
+        textAlign: 'center',
+        fontSize: 24,
+        color: '#3b82f6',
+        marginBottom: 10
+    },
+    input: {
+        backgroundColor: '#f5f5f5',
+        padding: 10,
         borderRadius: 10,
-        paddingVertical: 40, 
-        paddingHorizontal:20
-        },
+        textAlign: 'center',
+        marginTop: 10
+    },
+    boton: {
+        marginTop: 30,
+        backgroundColor: '#1048a4',
+        padding: 10,
+        borderRadius: 10,
+    },
+    botonTexto: {
+        color: '#FFF',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    }
 
 })
 
